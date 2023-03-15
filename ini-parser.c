2b81b54e213069be043976f18fc948fb35eb3e13
Jacob Millet <58485911+j-millet@users.noreply.github.com>
2023-03-15 19:37:09 +0100

    what is happening

--- ini-parser.c
+++ ini-parser.c
@@ -3,14 +3,41 @@
 
 struct section{
     char *name;
-    char *keys_str;
-    char *keys_int;
-    char *values_str;
-    char *values_int; 
+    char **keys;
+    char **values;
 };
 
+int strisnum(char *string)
+{
+    char c;
+    int i = 0;
+    while(c = string[i])
+    {
+        if (c<48 || c>57)
+        {
+            return 0;
+        }
+        i++;
+        
+    }
+    return 1;
+}
 
-
+int isvalidkey(char *string)
+{
+    char c;
+    int i = 0;
+    while(c = string[i])
+    {
+        if (!(c == 45 || (c > 47 && c < 58) ||(c > 64 && c < 91) ||(c > 96 && c < 123)))
+        {
+            return 0;
+        }
+        i++;
+        
+    }
+    return 1;
+}
 int getline(FILE* f, char **out)
 {
     int startpos = ftell(f);
@@ -30,6 +57,13 @@ int getline(FILE* f, char **out)
 
 int main(int argc, char const *argv[])
 {
+    /*
+    if(argc != 3){
+        printf("wrong amount of arguments (%d, expected 2)",argc-1);
+        exit(1);
+    }*/
+    int section_count = 0;
+    struct section * *sections;
     FILE* i = fopen("test.ini","r");
     char *line;
     int s;
@@ -37,12 +71,16 @@ int main(int argc, char const *argv[])
     {   
         if (line[0] == '[')
         {
-            char sname[s-2];
-            for (size_t i = 1; i < s-2; i++)
-            {
-                sname[i-1] = line[i];
-            }
-            
+            line[s-2] = ' '; //remove ] character
+            char *sname = (char *)malloc((s-1)*sizeof(char));
+            sscanf(line,"[%s",sname);
+            struct section newsec;
+            newsec.name = sname;
+
+            section_count++;
+            sections = (struct section* *)realloc(sections,sizeof(struct section *)*section_count);
+
+            sections[section_count-1] = &newsec;
             printf("NEW SECTION: [%s]\n",sname);
         }
         else if (line[0] != '\n' && line[0] != ';')
@@ -50,6 +88,7 @@ int main(int argc, char const *argv[])
             char key[s];
             char value[s];
             sscanf(line,"%s = %s",key,value);
+            if(!isvalidkey(key)){printf("invalid key: %s",key); exit(1);}
             printf("key:%s, value:%s\n",key,value);
         }
     }
